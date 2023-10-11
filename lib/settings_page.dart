e117030db3385f8bed727c5c85b2f9cae04a3e13
Sam Sanders <samuelsanders42@gmail.com>
2023-10-11 17:51:46 -0400

    Added a basic UI layout. Added a basic display for quiz questions.

---
+++ lib/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPage();
+}
+
+class _SettingsPage extends State<SettingsPage> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
